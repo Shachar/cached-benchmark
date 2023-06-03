a0cfd0a1042cb74c49e379ee68c6acee9d14ae0a
Shachar Shemesh <shachar@shemesh.biz>
2023-06-03 22:13:09 +0300

    Timing infra

--- benchmark.cpp
+++ benchmark.cpp
@@ -1,2 +1,12 @@
+#include "function.h"
+
+#include <chrono>
+
+using namespace std::chrono_literals;
+
+using Clock = std::chrono::steady_clock;
+using Timepoint = Clock::time_point;
+using Duration = Clock::duration;
+
 int main() {
 }
